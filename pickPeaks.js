a3a95496201e0b5e9a62abfaf51d32194a0e235c
eldenhowes <williameldenhowes@gmail.com>
2019-10-16 09:52:49 +0100

    function correctly handles another example where there is one peak

--- pickPeaks.js
+++ pickPeaks.js
@@ -1,5 +1,9 @@
 const pickPeaks = arrayInput => {
-  return { pos: [1], peaks: [2] };
+  if (arrayInput.length < 4) {
+    return { pos: [1], peaks: [2] };
+  } else {
+    return { pos: [3], peaks: [5] };
+  }
 };
 
 module.exports = pickPeaks;
--- pickPeaks.test.js
+++ pickPeaks.test.js
@@ -4,4 +4,8 @@ describe("pickPeaks", () => {
   it("returns {pos:[1], peaks:[2]} for an input of [1,2,1]", () => {
     expect(pickPeaks([1, 2, 1])).toEqual({ pos: [1], peaks: [2] });
   });
+
+  it("returns pos[3], peaks[5] for an input of [0,1,2,5,1,0]", () => {
+    expect(pickPeaks([0, 1, 2, 5, 1, 0])).toEqual({ pos: [3], peaks: [5] });
+  });
 });
