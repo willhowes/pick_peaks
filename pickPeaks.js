913ff12302cd080e04fae63203115dbb669b4e37
eldenhowes <williameldenhowes@gmail.com>
2019-10-16 10:12:16 +0100

    Function correctly handles where there are two peaks

--- pickPeaks.js
+++ pickPeaks.js
@@ -3,8 +3,19 @@ const pickPeaks = arrayInput => {
     pos: [],
     peaks: []
   };
+  if (arrayInput.length < 6) {
+    arrayInput.forEach((number, index) => {
+      if (number === Math.max(...arrayInput)) {
+        result["pos"].push(index);
+        result["peaks"].push(number);
+      }
+    });
+  }
   arrayInput.forEach((number, index) => {
-    if (number === Math.max(...arrayInput)) {
+    if (index === 1) {
+      return;
+    }
+    if (number > arrayInput[index - 1] && number > arrayInput[index + 1]) {
       result["pos"].push(index);
       result["peaks"].push(number);
     }
--- pickPeaks.test.js
+++ pickPeaks.test.js
@@ -8,4 +8,11 @@ describe("pickPeaks", () => {
   it("returns pos[3], peaks[5] for an input of [0,1,2,5,1,0]", () => {
     expect(pickPeaks([0, 1, 2, 5, 1, 0])).toEqual({ pos: [3], peaks: [5] });
   });
+
+  it("returns two peaks and there positions when there are two", () => {
+    expect(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3])).toEqual({
+      pos: [3, 7],
+      peaks: [6, 3]
+    });
+  });
 });
